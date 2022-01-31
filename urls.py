78cb09069b2894621e9b39e6f5c2a61d87f7e4fd
Mohammad Rezazadeh <mrprocs@gmail.com>
2022-01-31 21:32:07 +0330

    Change API url

--- urls.py
+++ urls.py
@@ -3,9 +3,9 @@ from dj_rest_auth.registration.views import RegisterView
 from django.urls import path, include, re_path
 
 urlpatterns = [
-    path('', include('dj_rest_auth.urls')),
-    path('registration/', include('dj_rest_auth.registration.urls')),
-    path('registration/', RegisterView.as_view(), name='account_signup'),
+    path('api/', include('dj_rest_auth.urls')),
+    path('api/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/registration/', RegisterView.as_view(), name='account_signup'),
     re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(),
             name='account_confirm_email'),
 
